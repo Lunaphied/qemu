6e49141437f88e159eef41c0fd7e26b53c298694
Iris Johnson <iris@modwiz.com>
2021-01-29 03:44:53 +0000

    hw/arm/apple-m1: Add development notes
    These are some slight tweaks to add a boot_args structure at 0x0 which will
    work since registers are reset to 0 and the bootloader is supposed to pass the address
    in register 0.
    
    The boot_args aren't initialized fully but it's enought to see that it's working.

--- hw/arm/apple-m1.c
+++ hw/arm/apple-m1.c
@@ -18,14 +18,31 @@
 #include "exec/exec-all.h"
 #include "hw/arm/apple-m1-soc.h"
 
-// FROM exynos4210.h
-DeviceState *exynos4210_uart_create(hwaddr addr,
-                                    int fifo_size,
-                                    int channel,
-                                    Chardev *chr,
-                                    qemu_irq irq);
+// Notes on this file:
+// - This file doesn't actually represent the M1 very well, it's more a platform designed and focused around m1n1
+// - The ARM IRQ pin is directly connected to the UART, which is helpful if you want to test the simplest 
+//   interrupts possible
+// - The structure of this file is based on the other board files I could find and me making my best guess
+//   unfortunately that doesn't get you very far, and there's some not so great stuff here too just to tie
+//   it all together to get an interesting demo
+// - The "M1" is emulated as if it's an SoC containing multiple Cortex-A72 cores, that's not true in reality,
+//   Apple has custom cores that have some special functionality that might be worth emulating, but now we have
+//   to edit the actual aarch64 emualtion logic (and we don't know the changes in full yet other than registers).
+//   I suspect with all Apple's custom registers and changes to existing registers, massive changes would be 
+//   required for true system emulation, this is something worth considering but not here, not right now.
+//
+// One idea for this is to do what's suggested below and literally pull in a device tree (converted to
+// not apple format) then use that to layout all the devices, creating stub regions of unimplemented registers
+// until they're created. Unfortunately it seems like nothing else in qemu does this, not even the most modern
+// code. So there's no framework, so that seems like it would be it's own project to prototype. 
+// So for now we hardcode and make a few M1 helpers
 
+// This is used in several other ARM platforms to help collect the addresses and give them meaningful names
+// during realization. These aren't correlated to the M1 hardware at all though it's just easier to set
+// them up like this during dev (the UART is the correct address for where it is when m1n1 loads though)
+// In reality this should be constructed from a hardware device tree provided from an M1 Mac
 static const struct MemMapEntry memmap[] = {
+    [BOOT_ARGS] =           {         0x0,     0x10000},
     [VIRT_MEM] =            {     0x10000,       8*GiB},
     [VIRT_UART] =           { 0x235200000,     0x10000},
 };
@@ -33,6 +50,9 @@ static const struct MemMapEntry memmap[] = {
 static void apple_m1_soc_init(Object *obj) {
     AppleM1SoCState *s = APPLE_M1_SOC(obj);
 
+    // not sure if this is the best way to create a CPU core, but we definitely want to use the A72 which
+    // seems to be the fanciest ARM core that qemu supports at the moment (which is probably the most like
+    // the M1's cores)
     object_initialize_child(obj, "maincpu", &s->maincore,
                             ARM_CPU_TYPE_NAME("cortex-a72"));
 }
@@ -42,6 +62,10 @@ static void apple_m1_soc_realize(DeviceState *dev, Error **errp)
     AppleM1SoCState *s = APPLE_M1_SOC(dev);
     //MemoryRegion *system_mem = get_system_memory();
 
+    // Yes there's only one CPU right now. This makes testing easier and I don't have an emulated interrupt
+    // controller so I think SMP stuff would be weird (probably not impossible since SEV exists though)
+    // But in truth we'd want to model the way the smp bringup works on actual hardware so lets not waste time
+    // on it now
     DeviceState* cpuobj = DEVICE(&s->maincore);
 
     // The apple M1 has no EL3 support but does have EL2, so these must be set for compatibility
@@ -104,12 +128,29 @@ static void apple_m1_init(MachineState *machine)
     object_property_add_child(OBJECT(machine), "soc", OBJECT(m1));
     qdev_realize_and_unref(DEVICE(m1), NULL, &error_abort);
 
+    // This initializes the memory region map
     MemoryRegion *sysmem = get_system_memory();
+
+    // doesn't this leak?
+    MemoryRegion *boot_args = g_new0(MemoryRegion, 1);
+    memory_region_init_rom(boot_args, NULL, "boot-args", memmap[BOOT_ARGS].size, &error_abort);
+    memory_region_add_subregion(sysmem, memmap[BOOT_ARGS].base, boot_args);
     memory_region_add_subregion(sysmem, memmap[VIRT_MEM].base, machine->ram);
+
+    // Used to add bootargs, yes I know this memory leaks but it doesn't matter
+    // at the moment, oddly adding the blob doesn't actually add it into the memory map
+    // which is annoying
+    void* boot_args_data = g_new0(uint8_t, 0x300);
+    // set the revision to something noticable
+    *(uint32_t*) ((uint8_t*) boot_args_data + 0) = 0xdead;
+    // set the memory size to the correct value
+    *(uint64_t*) ((uint8_t*) boot_args_data + 20) = memmap[VIRT_MEM].size;
+    rom_add_blob_fixed("boot-args", boot_args_data, 0x300, memmap[BOOT_ARGS].base);
+    g_free(boot_args_data);
 
     m1_boot_info.loader_start = memmap[VIRT_MEM].base;
     m1_boot_info.ram_size = machine->ram_size;
-    // HACK to try and get m1n1 loaded at a different address    
+
     arm_load_kernel(ARM_CPU(first_cpu), machine, &m1_boot_info);
     printf("Entry after load: %lx\n", m1_boot_info.entry);
 }
--- include/hw/arm/apple-m1-soc.h
+++ include/hw/arm/apple-m1-soc.h
@@ -1,14 +1,17 @@
-#ifndef HW_ARM_APPLE_M1_SOC_H
+#ifndef HW_ARM_APPLE_M1_SOC_H 
 #define HW_ARM_APPLE_M1_SOC_H
 
 #include "qom/object.h"
 #include "target/arm/cpu.h"
 
 enum {
-    VIRT_MEM,
-    VIRT_UART,
+    VIRT_MEM,   // A block of ram
+    VIRT_UART,  // The virtual uart (on real hardware this is on the type-C and multiplexed via USB-PD selection)
+    BOOT_ARGS   // We setup a block of ROM to make pretend boot arguments to match what the real hardware does
 };
 
+// We create an "m1 soc" but realistically the chip has a model, should we use that instead? this is pretty
+// high level emulation of the SoC so it might not really matter for our purposes. Probably should change it though.
 #define TYPE_APPLE_M1_SOC "apple-m1-soc"
 
 OBJECT_DECLARE_SIMPLE_TYPE(AppleM1SoCState, APPLE_M1_SOC)
